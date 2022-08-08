255ec1017e14bb0e0819716ff6570342777a128c
manati123 <silviu807@gmail.com>
2022-08-08 13:00:47 +0300

    initial commit

---
+++ Move/Move/MoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveApp.swift
+//  Move
+//
+//  Created by Silviu Preoteasa on 08.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
