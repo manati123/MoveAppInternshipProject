854afa156aa572c1b78dd0e91457d01d5c90aa77
manati123 <silviu807@gmail.com>
2022-08-26 09:35:49 +0300

    daily commit

--- Move/Move/Splash/SplashView.swift
+++ Move/Move/Splash/SplashView.swift
@@ -11,24 +11,41 @@ struct SplashView: View {
     let onFinished: () -> Void
     var body: some View {
         ZStack {
-            GeometryReader { geometry in
-                VStack{
-                    Spacer()
-                    HStack{
-                        
-                        ZStack{
-                            Image("CenterGroup")
-                            Image("ScooterPNG")
-                                .offset(x: -180)
-                            Image("MOVE")
-                        }
-                        .offset(x: -90, y: 0)
-                    }
-                    Spacer()
-                }
-            }
+//            GeometryReader { geometry in
+//                VStack{
+//                    Spacer()
+//                    HStack{
+//
+//                        ZStack{
+//                            Image("CenterGroup")
+//                                .renderingMode(.original)
+//                                .scaledToFit()
+//                            Image("ScooterPNG")
+//                                .renderingMode(.original)
+//                                .scaledToFit()
+//                                .offset(x: -180)
+//                            Image("MOVE")
+//                                .renderingMode(.original)
+//                                .scaledToFit()
+//                        }
+//                        .offset(x: -90, y: 0)
+//                    }
+//                    Spacer()
+//                }
+//            }
 //            .padding(/*@START_MENU_TOKEN@*/140.0/*@END_MENU_TOKEN@*/)
-            .background(Color("SplashBackground"))
+//            .background(Color("SplashBackground"))
+            ZStack {
+                Color.primaryPurple
+                            .ignoresSafeArea()
+                        Image("CenterGroup")
+                        Image("ScooterPNG")
+                            .offset(x: -116)
+                        Image("MOVE")
+                    }
+                    .onAppear() {
+                        onFinished()
+                    }
         }
         .onAppear() {
             onFinished()
